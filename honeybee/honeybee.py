60ba8f095212e412b5aa09ef1038d28cc6ad0fba
Engineero <engineerolabs@gmail.com>
2018-05-25 13:18:37 -0600

    Added honeybee.py, initial framework

---
+++ honeybee/honeybee.py
@@ -0,0 +1,18 @@
+""" Classes to implement the artificial bee colony algorithm. """
+
+
+class Colony:
+    """ Implements the artificial bee colony algorithm. """
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+
+# Maybe this should be a sub-class of Colony?
+class _Bee:
+    """ Defines a single bee in the colony. """
+    def __init__(self):
+        pass
+
