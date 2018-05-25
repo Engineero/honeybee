13d798128ce553906712ccca9c26416d7228a904
Engineero <engineerolabs@gmail.com>
2018-05-25 13:24:15 -0600

    Added arguments to Colony

--- honeybee/honeybee.py
+++ honeybee/honeybee.py
@@ -2,9 +2,19 @@
 
 
 class Colony:
-    """ Implements the artificial bee colony algorithm. """
-    def __init__(self):
-        pass
+    """ Implements the artificial bee colony algorithm.
+    
+    Args:
+        objective: objective function called by each bee at each food source.
+            Must return a "honey" value that will be maximized by the colony.
+        num_bees: number of employed bees in the colony, i.e. number of
+            solutions searched at each iteration of the algorithm.
+    
+    """
+    def __init__(self, objective, num_bees):
+        """ Initialize the colony. """
+        self.objective = objective
+        self.num_bees = num_bees
 
     def fit(self):
         pass
