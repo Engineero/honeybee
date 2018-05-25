7f40a038df6c8fdc96ec89f6df04d348bf37a2de
Engineero <Engineero@users.noreply.github.com>
2018-05-25 12:09:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 from setuptools import setup
+from distutils.util import convert_path
+from os import path
 
-setup(name='artificial_bee_colony')
+
+here = path.abspath(path.dirname(__file__))
+version_dict = {}
+version_path = convert_path('honeybee/_version.py')
+
+# Get the version number from the version path.
+with open(version_path, 'r') as ver_file:
+    exec(ver_file.read(), version_dict)
+
+# Get the long description from the README file.
+with open(path.join(here, 'README.md')) as a_file:
+    long_description = a_file.read()
+
+setup(name='honeybee',
+      version=version_dict['__version__'],
+      description='An artificial bee colony implementation in Python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Engineero')
