51d4a6f4a1ba087c016be0a7e1eac8af31097761
Engineero <engineerolabs@gmail.com>
2018-05-26 20:45:22 -0600

    Filling out some parameters

--- honeybee/honeybee.py
+++ honeybee/honeybee.py
@@ -9,20 +9,35 @@ class Colony:
             Must return a "honey" value that will be maximized by the colony.
         num_bees: number of employed bees in the colony, i.e. number of
             solutions searched at each iteration of the algorithm.
+        params: dictionary of optimization parameters and their ranges::
+
+            params = {'param_1': (min, max),
+                      'param_2': (min, max),
+                      ...,
+                      'param_n': (min, max)}
     
     """
-    def __init__(self, objective, num_bees):
+    def __init__(self, objective, num_bees: int, params: dict):
         """ Initialize the colony. """
         self.objective = objective
         self.num_bees = num_bees
+        self.params = params
+        self.is_initialized = False
 
     def fit(self):
         pass
+
+    def initialize(self):
+        self.is_initialized = True
 
 
 # Maybe this should be a sub-class of Colony?
 class _Bee:
     """ Defines a single bee in the colony. """
-    def __init__(self):
+    def __init__(self, position):
+        self.position = position
+        self.food = None  # objective function value at position
+
+    def evaluate(self):
         pass
 
